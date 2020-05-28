05e27e8e1bf4ecf3e79b73965fe38b0b00ea3622
taka1127 <taka07251127@gmail.com>
2020-05-28 21:48:34 +0900

    create-model

---
+++ src/main/java/com/example/expense/model/Category.java
@@ -0,0 +1,22 @@
+package com.example.expense.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name="category")
+public class Category {
+	@Id
+	private Long id;
+
+	private String name;
+	
+	
+
+}
---
+++ src/main/java/com/example/expense/model/User.java
@@ -0,0 +1,25 @@
+package com.example.expense.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
+@Table(name="users")
+public class User {
+
+	@Id
+	private String id;
+	
+	private String  name;
+	
+	private String email;
+
+}
