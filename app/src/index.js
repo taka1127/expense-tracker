269a79b6590588765943d4f44f05535c9eac5310
taka1127 <taka07251127@gmail.com>
2020-05-29 10:55:29 +0900

    springboot&react連携(category:id,nameの引き出し）

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Category from './Category';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Category />
   </React.StrictMode>,
   document.getElementById('root')
 );
